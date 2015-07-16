385bcfbf84b629c529a7f49c8aa7192d52fb72e2
MikhailFyodorov <mikhail.fyodorov.lanit@gmail.com>
2015-07-16 11:35:48 +0300

    ComRobotLinker
    Refactored

--- Sandbox/RoboConnection/ComRobotLinker.h
+++ Sandbox/RoboConnection/ComRobotLinker.h
@@ -5,18 +5,14 @@
 #include "com_connection.h"
 #include <robo_mavlink_test\mavlink.h>
 
-bool com_checksum(uint8_t *data, int32_t length, uint8_t com_ck_a, uint8_t com_ck_b); //Gets an array, length of its payload 
-//and checksums on the com-ports
-
 bool com_checksum(uint8_t *data, int32_t length); //Gets an array, length of its payload 
 
 class ComRobotLinker : public RobotLinker{
-private:
-	ComConnection* copterCom;
+
 public:
-
 	ComRobotLinker();
-
 	virtual void sendPacket(MavlinkPacket* packet);
 	virtual void openPort(QString name);
+private:
+	ComConnection* copterCom;
 };

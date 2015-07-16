05955f1dc5e6a93e75f01670333e5246bd51c40b
MikhailFyodorov <mikhail.fyodorov.lanit@gmail.com>
2015-07-16 11:33:11 +0300

    com_connection
    The class refactored

--- Sandbox/RoboConnection/com_connection.h
+++ Sandbox/RoboConnection/com_connection.h
@@ -6,20 +6,14 @@
 #include <QtSerialPort\QSerialPortInfo>
 
 class ComConnection{
-
-private:
-	QSerialPort serial;
-
 public:
-	
 	ComConnection();
 	void openPort(QString name);
 	void Write(unsigned char* message, int length);
-
 	QByteArray readPacket();
-
 	void getParamList();
 private:
+	QSerialPort serial;
 	bool com_checksum(uint8_t *data, int32_t length, uint8_t com_ck_a, uint8_t com_ck_b); //Gets an array, length of it payload 
 	
 };
